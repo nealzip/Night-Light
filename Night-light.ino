b00a85c29b55cfcd2f5789057f57b1e5ac79aa41
Neal Zipper <Neal@nealZipper.com>
2016-06-20 19:13:34 -0400

    Update

--- Night-light.ino
+++ Night-light.ino
@@ -1,3 +1,5 @@
+// This is my inital Commit
+
 // This is a demonstration on how to use an input device to trigger changes on your neo pixels.
 // You should wire a momentary push button to connect from ground to a digital IO pin.  When you
 // press the button it will change to a new pixel animation.  Note that you need to press the
